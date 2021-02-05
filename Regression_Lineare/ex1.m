a6759adf7a3d17c8b4c3e337995487082f3dbc39
Koussaila Kadi <66485079+Koussailakadi@users.noreply.github.com>
2021-02-05 22:26:21 +0100

    Update ex1.m

--- Regression_Lineare/ex1.m
+++ Regression_Lineare/ex1.m
@@ -2,11 +2,7 @@
 
 %  Instructions
 %  ------------
-%
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions
-%  in this exericse:
-%
+
 %     warmUpExercise.m
 %     plotData.m
 %     gradientDescent.m
@@ -16,9 +12,7 @@
 %     featureNormalize.m
 %     normalEqn.m
 %
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
+
 % x refers to the population size in 10,000s
 % y refers to the profit in $10,000s
 %
