a918719e989ebeafc742bc9b50f321609a2f4194
Koussaila Kadi <66485079+Koussailakadi@users.noreply.github.com>
2021-02-05 22:23:15 +0100

    Update featureNormalize.m

--- Regression_Lineare/featureNormalize.m
+++ Regression_Lineare/featureNormalize.m
@@ -10,21 +10,8 @@ X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
+
+
 for i = 1:length(size(X))
     mu(i)= mean(X(:,i));
     sigma(i)=max(X(:,i));
